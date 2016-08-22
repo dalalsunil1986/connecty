3c853eab92bdb9985d060d21809e96b1c8507d11
Givantha Kalansuriya <GivanthaK@brandix.com>
2016-08-22 09:04:11 +0530

    add passport controller and auth changes.

--- server.js
+++ server.js
@@ -2,7 +2,13 @@
 var express = require('express');
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser');
+var passport = require('passport');
+
+// Controller dependency
+var authController = require('./app/controllers/auth');
+var userController = require('./app/controllers/user');
 var contactController = require('./app/controllers/contact');
+
 
 // Create our Express application
 var connectyServer = express();
@@ -18,19 +24,28 @@ mongoose.connect('mongodb://localhost:27017/connecty');
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
 
+// Use the passport package in our application
+connectyServer.use(passport.initialize());
+
 // Create our Express router
 var router = express.Router();
 
+// Create endpoint handlers for /users
+router.route('/users')
+  .post(userController.postUsers)
+  .get(authController.isAuthenticated, userController.getUsers);
+
 // Create endpoint handlers for /contacts
 router.route('/contacts')
-  .post(contactController.postContacts)
-  .get(contactController.getContacts);
+  .post(authController.isAuthenticated, contactController.postContacts)
+  .get(authController.isAuthenticated, contactController.getContacts);
 
 // Create endpoint handlers for /contacts/:beer_id
 router.route('/contacts/:contact_id')
-  .get(contactController.getContact)
-  .put(contactController.putContact)
-  .delete(contactController.deleteContact);
+  .get(authController.isAuthenticated, contactController.getContact)
+  .put(authController.isAuthenticated, contactController.putContact)
+  .delete(authController.isAuthenticated, contactController.deleteContact);
+
 
 // Initial dummy route for testing
 // http://localhost:3000/api
